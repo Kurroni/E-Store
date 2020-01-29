327bae35e3c55338f3fdc552adaacfe17574dd24
MichalR <mrudny@gmail.com>
2020-01-29 16:30:04 +0100

    render Modal component

--- src/components/Details.js
+++ src/components/Details.js
@@ -37,6 +37,7 @@ export default class Details extends Component {
                                     </Link>
                                     <ButtonContainer cart disabled={inCart?true:false} onClick={()=> {
                                         value.addToCart(id);
+                                        value.openModal(id);
                                     }}>
                                     {inCart ? "in cart" : "add to cart"}
                                     </ButtonContainer>
