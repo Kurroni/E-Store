f60a5f49a576fa355e084fac7c72a204083c626b
MichalR <mrudny@gmail.com>
2020-02-05 00:01:33 +0100

    make UI of cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,8 @@ import Title from '../Title';
 import CartColumns from './CartColumns';
 import EmptyCart from "./EmptyCart";
 import {ProductConsumer} from "../../context";
+import CartList from './CartList';
+import CartTotals from './CartTotals'
 
 export default class Cart extends Component {
     render() {
@@ -16,7 +18,9 @@ export default class Cart extends Component {
                         return(
                             <React.Fragment>
                             <Title name="your" title="cart"/>
-                                <CartColumns/>
+                            <CartColumns/>
+                            <CartList value={value}/>
+                            <CartTotals value={value}/>
                             </React.Fragment>
                             );
                     } else {
