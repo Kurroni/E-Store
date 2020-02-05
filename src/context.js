2397988ff7006d71e61f0ee68d219a0530ba2f22
MichalR <mrudny@gmail.com>
2020-02-05 10:29:55 +0100

    make error page 404

--- src/context.js
+++ src/context.js
@@ -69,15 +69,54 @@ class ProductProvider extends Component {
            return {modalOpen:false} 
         })
     };
-    increment = (id) => {
-        console.log("this is increment method")
-    }
-    decrement = (id) => {
-        console.log("this is decrement method")
-    }
-    removeItem = (id) => {
-        console.log("item removed")
-    }
+    increment = id => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count + 1;
+        product.total = product.count * product.price;
+
+        this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotals()})
+    };
+    decrement = id => {
+        let tempCart = [...this.state.cart];
+        const selectedProduct = tempCart.find(item=>item.id === id);
+        const index = tempCart.indexOf(selectedProduct);
+        const product = tempCart[index];
+
+        product.count = product.count - 1;
+
+        if (product.count === 0){
+            this.removeItem(id)
+        } else {
+            product.total = product.count * product.price;
+            this.setState(()=>{return{cart:[...tempCart]}},()=>{this.addTotals()})
+        };        
+    };
+    removeItem = id => {
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item=>item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(()=>{
+            return {
+                cart:[...tempCart],
+                products:[...tempProducts]
+            }
+        }, ()=>{
+            this.addTotals();
+        }
+        );
+    };
     clearCart = ()=>{
         this.setState(()=>{
             return {cart:[]};
